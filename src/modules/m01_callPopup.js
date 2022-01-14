745e01183a20eaa413ebd0e937c376c795953da8
peslx <90468331+peslx@users.noreply.github.com>
2022-01-15 01:33:18 +0900

    Реализован вызов popup обратного звонка

--- src/modules/m01_callPopup.js
+++ src/modules/m01_callPopup.js
@@ -1,3 +1,30 @@
 export const m01_callPopup = () => {
-  console.log("m01_callPopup");
+  // console.log("m01_callPopup");
+  const callPopups = document.querySelectorAll("a[href='#callback']");
+  const overlay = document.querySelector(".overlay");
+  const headerModal = document.querySelector(".header-modal");
+  const closeBtn = headerModal.querySelector(".header-modal__close");
+
+  const reveal = (elem) => {
+    elem.style.display = "block";
+    overlay.style.display = "block";
+  };
+
+  const hide = (elem) => {
+    elem.style.display = "none";
+    overlay.style.display = "none";
+  };
+
+  document.addEventListener("click", (e) => {
+    callPopups.forEach((a) => {
+      if (e.target === a) {
+        e.preventDefault;
+        reveal(headerModal);
+      } else if (e.target === closeBtn || e.target === overlay) {
+        hide(headerModal);
+      }
+    });
+  });
+
+  //
 };
