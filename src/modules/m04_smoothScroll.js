935d4c79dcac2cc924d360462df034fdcb9639c7
peslx <90468331+peslx@users.noreply.github.com>
2022-01-17 23:34:48 +0900

    Реализован плавный скрол при клике на 'возвратку'

--- src/modules/m04_smoothScroll.js
+++ src/modules/m04_smoothScroll.js
@@ -1,3 +1,33 @@
 export const m04_smoothScroll = () => {
-  console.log("m04_smoothScroll");
+  const scrollBtn = document.querySelector(".smooth-scroll");
+  const offer = document.getElementById("offer");
+
+  const hideScrollBtn = () => {
+    scrollBtn.style.display = "none";
+    scrollBtn.style.opacity = 0;
+  };
+
+  const showScrollBtn = () => {
+    scrollBtn.style.display = "";
+    scrollBtn.style.opacity = "";
+  };
+
+  hideScrollBtn();
+
+  document.addEventListener("scroll", () => {
+    if (window.scrollY >= offer.offsetTop + offer.offsetHeight) {
+      showScrollBtn();
+    } else if (window.scrollY <= offer.offsetHeight - offer.offsetTop) {
+      hideScrollBtn();
+    }
+  });
+
+  scrollBtn.addEventListener("click", (e) => {
+    console.dir(benefits);
+    e.preventDefault();
+    window.scrollBy({
+      top: -window.scrollY,
+      behavior: "smooth",
+    });
+  });
 };
