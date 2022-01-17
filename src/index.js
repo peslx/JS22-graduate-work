4f8bb2c383adff71c92083e5803e71e6ca1bbaeb
peslx <90468331+peslx@users.noreply.github.com>
2022-01-17 22:48:06 +0900

    Реализован таймер акции

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import { m03_timer } from "./modules/m03_timer";
 
-m03_timer();
+m03_timer("January 21, 2022 21:00");
--- src/modules/m03_timer.js
+++ src/modules/m03_timer.js
@@ -1,3 +1,46 @@
-export const m03_timer = () => {
-  console.log("m03_timer");
+export const m03_timer = (deadline) => {
+  const timer = document.querySelector(".countdown-text");
+  const timerDays = document.querySelector(".count_1 > span");
+  const timerHours = document.querySelector(".count_2 > span");
+  const timerMinutes = document.querySelector(".count_3 > span");
+  const timerSeconds = document.querySelector(".count_4 > span");
+
+  const getTimeToDeadline = (deadline) => {
+    let stopDate = new Date(deadline).getTime();
+    let nowDate = new Date().getTime();
+    let remTime = Math.floor((stopDate - nowDate) / 1000);
+    let days = Math.floor(remTime / 60 / 60 / 24);
+    let hours = Math.floor((remTime / 60 / 60) % 24);
+    let minutes = Math.floor((remTime / 60) % 60);
+    let seconds = Math.floor(remTime % 60);
+    return { days, hours, minutes, seconds, remTime };
+  };
+
+  const refreshTime = () => {
+    let time = getTimeToDeadline(deadline);
+    timerDays.textContent = prop(time.days);
+    timerHours.textContent = prop(time.hours);
+    timerMinutes.textContent = prop(time.minutes);
+    timerSeconds.textContent = prop(time.seconds);
+
+    if (time.remTime <= 0) {
+      clearInterval(timerID);
+      setInterval(stopMsg, 2000);
+      timerDays.textContent = "00";
+      timerHours.textContent = "00";
+      timerMinutes.textContent = "00";
+      timerSeconds.textContent = "00";
+    }
+  };
+
+  const prop = (num) => {
+    let res = String(num);
+    if (res.length < 2) res = `0${res}`;
+    return res;
+  };
+  const stopMsg = () => {
+    timer.innerHTML = "Акция завершена!";
+  };
+  refreshTime();
+  let timerID = setInterval(refreshTime, 1000);
 };
