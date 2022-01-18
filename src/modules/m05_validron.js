34773d2f6ba583b22d4afdab5d575559c1727275
peslx <90468331+peslx@users.noreply.github.com>
2022-01-19 01:25:01 +0900

    Начат скрипт обработки форм

--- src/modules/m05_validron.js
+++ src/modules/m05_validron.js
@@ -1,3 +1,27 @@
 export const m05_validron = () => {
-  console.log("m05_validron");
+  class Validron {
+    constructor() {
+      this.forms = document.querySelectorAll("form");
+    }
+  }
+
+  const forms = document.querySelectorAll("form");
+  console.log(forms);
+  document.addEventListener("submit", (e) => {
+    forms.forEach((f) => {
+      if (e.target === f) {
+        e.preventDefault();
+        console.dir(f);
+
+        f.querySelectorAll("input").forEach((i) => {
+          console.log(`${i.name}`);
+        });
+
+        const formData = new FormData(f);
+        for (const [name, val] of formData) {
+          console.log(name + " : " + val);
+        }
+      }
+    });
+  });
 };
